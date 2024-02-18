bc536f5398fa93988aaa960a5cc44f366af1c2ca
shaoyunlu <shaoyunlu@126.com>
2024-02-18 17:44:04 +0800

    1

--- src/page/base/carousel.vue
+++ src/page/base/carousel.vue
@@ -1,66 +1,84 @@
 <template>
-    <draggable v-model="items" v-bind="dragOptions" class="flex-container" item-key="id">
-      <template #item="{ element }">
-        <div class="flex-item" :key="element.id">{{ element.name }}</div>
-      </template>
-    </draggable>
-  </template>
-  
-  <script>
-  import { computed, defineComponent, ref } from 'vue';
-  import draggable from 'vuedraggable';
-  
-  export default defineComponent({
-    components: {
-      draggable,
-    },
-    setup() {
-      const items = ref([
-        { id: 1, name: 'Item 1' },
-        { id: 2, name: 'Item 2' },
-        { id: 3, name: 'Item 3' },
-        { id: 4, name: 'Item 4' },
-        { id: 5, name: 'Item 5' },
-        { id: 6, name: 'Item 6' },
-        { id: 7, name: 'Item 7' },
-        { id: 8, name: 'Item 8' },
-      ]);
-  
-     const dragOptions = computed(()=>{
-        return {
-            animation: 200,
-            group: "description",
-            disabled: false,
-            ghostClass: "ghost"
-        }
-     })
 
-      return { items ,dragOptions };
-    },
-  });
-  </script>
-  
-  <style>
- .flex-container {
-  display: flex;
-  flex-direction: row;
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
+  <div>
+      <xmv-upload :file-list="fileList" drag :limit="1">
+          <xmv-icon name="uploadFilled" class="xmv-icon--upload"></xmv-icon>
+          <div class="xmv-upload__text">
+              点击上传系统 logo 
+          </div>
+      </xmv-upload>
+  </div>
 
-.flex-item {
-  transition: transform 0.3s ease;
-  margin: 5px;
-  padding: 10px;
-  background-color: #f2f2f2;
-  border: 1px solid #ccc;
-  cursor: grab;
-}
+  <draggable v-model="items" v-bind="dragOptions" class="flex-container" item-key="id">
+    <template #item="{ element }">
+      <div class="flex-item" :key="element.id">
+        <xmv-image :src="element.url"></xmv-image>
+      </div>
+    </template>
+  </draggable>
+</template>
 
-/* Optional: Add additional styles for dragging state */
-.flex-item.dragging {
-  opacity: 0.6;
-}
-  </style>
-  
+<script>
+import { computed, defineComponent, onMounted, ref } from 'vue'
+import {loadingOpen,loadingClose,messageDialog,confirmDialog} from 'util/dom'
+import draggable from 'vuedraggable'
+import http from 'util/http'
+
+export default defineComponent({
+  components: {
+    draggable,
+  },
+  setup() {
+    const items = ref([]);
+
+   const dragOptions = computed(()=>{
+      return {
+          animation: 200,
+          group: "description",
+          disabled: false,
+          ghostClass: "ghost"
+      }
+   })
+
+   const fetchData = ()=>{
+      loadingOpen()
+      http.get('base/carousel/list').then(data=>{
+          loadingClose()
+          items.value = data
+      })
+   }
+
+   onMounted(()=>{
+      fetchData()
+   })
+
+    return { items ,dragOptions };
+  },
+});
+</script>
+
+<style>
+  .flex-container {
+    flex-direction: row;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .flex-item {
+    transition: transform 0.3s ease;
+    width: 320px;
+    height: 180px;
+    margin: 5px;
+    padding: 10px;
+    background-color: #f2f2f2;
+    border: 1px solid #ccc;
+    cursor: grab;
+    float: left;
+  }
+
+  /* Optional: Add additional styles for dragging state */
+  .flex-item.dragging {
+    opacity: 0.6;
+  }
+</style>
