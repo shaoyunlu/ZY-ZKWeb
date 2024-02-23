426a6fe66c606aea68db3338fbc32c7513984fd1
shaoyunlu <shaoyunlu@126.com>
2024-02-23 10:35:10 +0800

    1

--- src/page/base/carousel.vue
+++ src/page/base/carousel.vue
@@ -1,18 +1,20 @@
 <template>
-
   <div class="carousel-upload">
-      <xmv-upload :file-list="fileList" drag :limit="1" @uploadDone="handleUploadDone">
-          <xmv-icon name="uploadFilled" class="xmv-icon--upload"></xmv-icon>
-          <div class="xmv-upload__text">
-              点击上传轮播图
-          </div>
-      </xmv-upload>
+    <xmv-upload :file-list="fileList" drag :limit="1" @uploadDone="handleUploadDone">
+      <xmv-icon name="uploadFilled" class="xmv-icon--upload"></xmv-icon>
+      <div class="xmv-upload__text">
+        点击上传轮播图
+      </div>
+    </xmv-upload>
   </div>
 
-  <draggable v-model="items" v-bind="dragOptions" class="flex-container" item-key="id">
+  <draggable v-model="items" v-bind="dragOptions" class="flex-container" item-key="id" @end="handleDragEnd">
     <template #item="{ element }">
       <div class="flex-item" :key="element.id">
         <xmv-image :src="element.url"></xmv-image>
+        <div class="flex-item-mask">
+          <xmv-button @click="handleItemDelete">删除</xmv-button>
+        </div>
       </div>
     </template>
   </draggable>
@@ -20,7 +22,7 @@
 
 <script>
 import { computed, defineComponent, onMounted, ref } from 'vue'
-import {loadingOpen,loadingClose} from 'util/dom'
+import { loadingOpen, loadingClose } from 'util/dom'
 import draggable from 'vuedraggable'
 import http from 'util/http'
 
@@ -32,84 +34,114 @@ export default defineComponent({
     const items = ref([]);
     const fileList = ref([])
 
-   const dragOptions = computed(()=>{
+    const dragOptions = computed(() => {
       return {
-          animation: 200,
-          group: "description",
-          disabled: false,
-          ghostClass: "ghost"
+        animation: 200,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost"
       }
-   })
+    })
 
-   const fetchData = ()=>{
+    const fetchData = () => {
       loadingOpen()
-      http.get('base/carousel/list').then(data=>{
-          loadingClose()
-          items.value = data
+      http.get('base/carousel/list').then(data => {
+        loadingClose()
+        items.value = data
       })
-   }
+    }
 
-   const handleUploadDone = (formFileList)=>{
+    const handleUploadDone = (formFileList) => {
       let formFile = formFileList[0]
       __addPic(formFile)
-   }
+    }
 
-   const __addPic = (formFile)=>{
-      const formData = new FormData()
-      formData.append('image' ,file)
-
-      http.post('pic/upload' ,formData ,{
-        headers : {'Content-Type' : 'multipart/form-data'}
-      }).then(res=>{
-        console.log(res)
+    const handleDragEnd = ()=>{
+      loadingOpen()
+      http.post('base/carousel/order/adjust' ,items.value).then(res=>{
+        loadingClose()
       })
-   }
+    }
 
-   const __adjustOrder = ()=>{
+    const handleItemDelete = ()=>{
+      console.log(1)
+    }
 
-   }
+    const __addPic = (formFile) => {
+      loadingOpen()
+      const formData = new FormData()
+      formData.append('image', formFile.obj)
+      http.post('base/carousel/pic/upload', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      }).then(res => {
+        loadingClose()
+        items.value.push({ id: res.is, url: res.url, order: res.order })
+      }).catch(err => {
+        loadingClose()
+      })
+    }
 
-   const __delPic = ()=>{
+    const __adjustOrder = () => {
 
-   }
+    }
 
-   onMounted(()=>{
+    const __delPic = () => {
+
+    }
+
+    onMounted(() => {
       fetchData()
-   })
+    })
 
-    return { items ,dragOptions ,fileList ,handleUploadDone};
+    return { items, dragOptions, fileList, handleUploadDone ,handleDragEnd ,handleItemDelete};
   },
 });
 </script>
 
 <style lang="less">
-  .flex-container {
-    flex-direction: row;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-  }
+.flex-container {
+  flex-direction: row;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
 
-  .flex-item {
-    transition: transform 0.3s ease;
-    width: 180px;
-    height: 320px;
-    margin: 5px;
-    background-color: #f2f2f2;
-    border: 1px solid #ccc;
-    cursor: grab;
-    float: left;
-  }
+.flex-item-mask{
+  position: absolute;
+  left: 0;
+  top: 0;
+  width : 100%;
+  height: 100%;
+  opacity: 0;
+}
 
-  /* Optional: Add additional styles for dragging state */
-  .flex-item.dragging {
-    opacity: 0.6;
-  }
+.flex-item {
+  position: relative;
+  transition: transform 0.3s ease;
+  width: 180px;
+  height: 320px;
+  margin: 5px;
+  background-color: #f2f2f2;
+  border: 1px solid #ccc;
+  cursor: grab;
+  float: left;
+}
 
-  .carousel-upload{
-    width : 180px;
-    .xmv-upload-list{
-      display: none;
-    }
+.flex-item:hover .flex-item-mask{
+  opacity: 1;
+  background-color: rgba(0,0,0,0.5);
+}
+
+/* Optional: Add additional styles for dragging state */
+.flex-item.dragging {
+  opacity: 0.6;
+}
+
+.carousel-upload {
+  width: 180px;
+
+  .xmv-upload-list {
+    display: none;
   }
+}
 </style>
